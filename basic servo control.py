48ac0582dd43e93d040a7b96a8826a4b7e78e092
intermediateprogrammer <tbhidkagoodusername@gmail.com>
2024-07-04 14:39:19 +0530

    Update basic servo control.py

--- basic servo control.py
+++ basic servo control.py
@@ -4,14 +4,20 @@ import pulseio
 import time
 from adafruit_motor import servo
 servopower = digitalio.DigitalInOut(board.D22)
-pin23 = digitalio.DigitalInOut(board.D23)
-#Access gpio pin 22 and 23
-
 servopower.direction = digitalio.Direction.OUTPUT
 #Gpio pin 22 is set to output
+pwm5 = pulseio.PWMOut(board.D5, frequency=50)
 pwm23 = pulseio.PWMOut(board.D23, frequency=50)
-my_servo = servo.Servo(pwm23)
-my_servo.angle = 90
+pwm6 = pulseio.PWMOut(board.D6, frequency=50)
+kitsune_arm1 = servo.Servo(pwm5)
+kitsune_arm2 = servo.Servo(pwm23)
+kitsune_head = servo.Servo(pwm6)
+kitsune_arm1.angle = 90
+time.sleep(2)
+kitsune_arm2.angle = 90
+time.sleep(2)
+kitsune_head.angle = 90
+
 
 
     
